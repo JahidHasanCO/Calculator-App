2ddc91ca4bc7de7e972c178ace9e0518bd57ce4f
Jahid hasan <vdjsovaj@gmail.com>
2021-07-10 14:13:39 +0600

    bug fix

--- app/src/main/java/Dev/JahidHasanCo/calculator/MainActivity.kt
+++ app/src/main/java/Dev/JahidHasanCo/calculator/MainActivity.kt
@@ -2,27 +2,42 @@ package Dev.JahidHasanCo.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import org.mariuszgromada.math.mxparser.Expression
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
 
     button_clear.setOnClickListener {
         input.text = ""
         output.text = ""
     }
 
-    button_bracket_left.setOnClickListener {
-        input.text = addToInputText("(")
+        button_bracket.setOnClickListener {
+
+                input.text = addToInputText("(")
+
     }
-    button_bracket_right.setOnClickListener {
-        input.text = addToInputText(")")
+        button_bracket_r.setOnClickListener {
+
+                input.text = addToInputText(")")
+
     }
+
+        button_croxx.setOnClickListener {
+           val removedLast = input.text.toString().dropLast(1)
+            input.text = removedLast
+        }
+
     button_0.setOnClickListener {
         input.text = addToInputText("0")
     }
@@ -62,6 +77,7 @@ class MainActivity : AppCompatActivity() {
     button_multiply.setOnClickListener {
         input.text = addToInputText("×") // ALT + 0215
     }
+
     button_subtraction.setOnClickListener {
         input.text = addToInputText("-")
     }
@@ -75,7 +91,8 @@ class MainActivity : AppCompatActivity() {
 }
 
 private fun addToInputText(buttonValue: String): String {
-    return "${input.text}$buttonValue"
+
+    return input.text.toString() + "" + buttonValue
 }
 
 private fun getInputExpression(): String {
@@ -90,7 +107,7 @@ private fun showResult() {
         val result = Expression(expression).calculate()
         if (result.isNaN()) {
             // Show Error Message
-            output.text = "Error"
+            output.text = ""
             output.setTextColor(ContextCompat.getColor(this, R.color.red))
         } else {
             // Show Result
@@ -99,7 +116,7 @@ private fun showResult() {
         }
     } catch (e: Exception) {
         // Show Error Message
-        output.text = "Error"
+        output.text = ""
         output.setTextColor(ContextCompat.getColor(this, R.color.red))
     }
 }
